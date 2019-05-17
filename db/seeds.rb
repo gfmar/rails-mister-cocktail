1022dcb0261bbc221187b779ede5e9d495251faf
Gustaf Folkmar <gustaf@folkmar.se>
2019-05-17 12:05:39 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+img_url = 'https://source.unsplash.com/random?cocktail'
 
 puts 'Wiping entire DB'
 Cocktail.destroy_all
 Ingredient.destroy_all
 
 puts 'Adding cocktails...'
-Cocktail.create(name: "Alex's gross protien vegan chocolate shake")
-Cocktail.create(name: "Joe's cup of joe")
-Cocktail.create(name: "Marc's ice tea")
+Cocktail.create(name: "Alex's gross protien vegan chocolate shake", photo: img_url)
+Cocktail.create(name: "Joe's cup of joe", photo: img_url)
+Cocktail.create(name: "Marc's ice tea", photo: img_url)
 
 
 puts 'Adding ingredients...'
